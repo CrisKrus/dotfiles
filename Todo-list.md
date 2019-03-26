12caa4ba5611d74ff3a934a46e661f2e0150cd64
Cristian Suárez Vera <cristian.suarez.ulpgc@gmail.com>
2019-03-26 22:00:22 +0000

    first commit

---
+++ Todo-list.md
@@ -0,0 +1,38 @@
+Software to install
+================================================================================
+
+- [x] vim
+- [x] git
+- [x] docker
+- [x] zsh
+- [x] oh my zsh
+- [x] spotify
+- [x] obs
+- [x] dropbox
+- [x] keep
+- [x] vlc
+- [x] customize OS
+    - [x] dock
+    - [x] top bar
+    - [ ] WIP show system realtime resources
+- [x] moeditor (md viewer)
+
+- [ ] calendar
+- [ ] WIP autojump
+- [ ] thunderbird
+- [ ] trello
+- [ ] infinite bash history
+- [x] whatsapp
+- [ ] telegram
+- [ ] discord
+
+# Work things
+
+- [ ] slack
+- [ ] IDEs
+    - [ ] Intellij
+- [ ] zoom
+- [ ] python environment
+- [ ] nvm
+- [ ] rvm
+- [x] npm
---
+++ programs.sh
@@ -0,0 +1,66 @@
+#/bin/bash!
+
+########## Add repositories ##########
+
+# spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# OBS
+sudo add-apt-repository ppa:obsproject/obs-studio
+
+# KeePass
+sudo add-apt-repository ppa:eugenesan/ppa
+
+##########  Update system   ##########
+sudo apt update
+sudo apt upgrade
+
+########## Install software ##########
+sudo apt install vim -y
+sudo apt install git -y
+sudo apt install docker -y
+sudo apt install spotify-client -y
+sudo apt install obs-studio -y
+sudo apt install keepass2 -y
+sudo apt install vlc -y
+sudo apt install npm -y
+
+### Oh my zsh ###
+sudo apt install zsh -y
+# take care this link can change any time
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# after that to use zsh by default zsh
+chsh -s $(which zsh)
+
+### Autojump ###
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
+
+### Custormize dock ###
+gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#d3d3d3'
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items false
+
+### Customize interface ###
+gsettings set org.gnome.desktop.interface clock-show-date true
+# show battery percentage in case that you use laptop
+# gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+### WIP resources status bar ###
+# https://github.com/LukeSmithxyz/voidrice/tree/master/.scripts/statusbar
+# https://www.youtube.com/watch?v=MNwpdqD_I8Y&t=799s
+
+### Dropbox ###
+# this option can't run on start up
+# cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+# ~/.dropbox-dist/dropboxd &
+
+### Moeditor ###
+# source to download .deb
+# https://github.com/Moeditor/Moeditor/releases/download/v0.2.0-beta/moeditor_0.2.0-1_amd64.deb
